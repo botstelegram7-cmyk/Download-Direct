9720a1ebb859f73e53c0a1cf1df60031dda1d215
botstelegram7-cmyk <botstelegram7@gmail.com>
2026-03-04 11:16:47 +0530

    Create start.py

---
+++ plugins/start.py
@@ -0,0 +1,11 @@
+from pyrogram import filters, Client
+from pyrogram.types import Message
+from client import app
+
+@app.on_message(filters.command("start") & filters.outgoing)
+async def start_cmd(client: Client, message: Message):
+    await message.reply("🚀 **SerenaBot Started!**\nUniversal Downloader - Send URL!")
+
+@app.on_message(filters.command("ping") & filters.outgoing)
+async def ping(client: Client, message: Message):
+    await message.reply("🏓 Pong!")
